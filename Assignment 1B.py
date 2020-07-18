f1bcf46f0d1bda994398fed66c8f6ddeda9c151d
Rutuja-Patil <68053911+PatilRutuja2009@users.noreply.github.com>
2020-07-18 13:13:55 +0530

    Add files via upload

---
+++ Assignment 1B.py
@@ -0,0 +1,23 @@
+#Assignment no-1 B
+
+
+list=[9,6,3,1,5]
+print(list)
+
+list.insert(4,2)
+print(list)
+
+list.remove(3)
+print(list)
+
+list.insert(8,60)
+print(list)
+
+list.reverse()
+print(f"reverse list{list}")
+
+list.sort()
+print(f"sorted list{list}")
+
+list.pop(-1)
+print(f"pop function of list{list}")
